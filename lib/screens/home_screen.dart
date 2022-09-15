81732785a362b7cdc85758b0071fd327fd793cb8
jerinzech <jerinzjames@gmail.com>
2022-09-15 07:39:21 +0530

    added some UI elements

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,12 +17,113 @@ class HomeScreen extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
+        appBar: const _CustomAppBar(),
+        bottomNavigationBar: const _CustomNavBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const _DIscoverMusic(),
+            ],
+          ),
         ),
-        body: Container(),
       ),
     );
   }
+}
+
+class _DIscoverMusic extends StatelessWidget {
+  const _DIscoverMusic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Welcome", style: Theme.of(context).textTheme.bodyLarge),
+          const SizedBox(height: 5.0),
+          Text(
+            "Enjoy your favourite music",
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Search',
+                hintStyle: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Colors.grey.shade400),
+                prefixIcon: Icon(Icons.search, color: Colors.grey.shade400),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide.none,
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomNavBar extends StatelessWidget {
+  const _CustomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.deepPurple.shade800,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border), label: "Favorite"),
+          BottomNavigationBarItem(icon: Icon(Icons.play_circle), label: "Play"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.people_outline), label: "Profile"),
+        ]);
+  }
+}
+
+class _CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const _CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: const Icon(Icons.grid_view_rounded),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 20.0),
+          child: const CircleAvatar(
+            backgroundImage: AssetImage('lib/assets/images/skull.jpg'),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(56.0);
 }
